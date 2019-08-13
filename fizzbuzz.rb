013dbeef447267d06032f8e905d90acaad36fddc
honest-cast-1403 <nganjani@protonmail.com>
2019-08-13 20:59:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
-def fizzbuzz
+def fizzbuzz(number)
+  if number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  elsif number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  else
+    returns nil
 end
